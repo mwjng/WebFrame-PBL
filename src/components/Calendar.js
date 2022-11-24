5dc998ed4dae61d736ed4283f8b53a36d01b4d6e
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2022-11-24 16:39:29 +0900

    css+cal 4
    dd

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,17 +1,16 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Component } from 'react';
 import interactionPlugin from "@fullcalendar/interaction";
-import productData from "../data/product-data.json";
-import {firestore} from "../firebase_config"
-import styled from "@emotion/styled";
+import { firestore } from "../firebase_config"
+import { getDocs } from "firebase/firestore"
 import '../style/style.css'
 
-export const Main=()=>{ //시작페이지
-
-  return(
+export const Main = () => { //시작페이지
+  return (
     <div className='mainPage'>
       <h1>TEAM : 2 0 1 8</h1>
     <h1><Link to ="calendar">C A L E N D A R</Link></h1>
@@ -19,74 +18,80 @@ export const Main=()=>{ //시작페이지
   );
 }
 
-
-export const Error=()=>{ //에러페이지
-    const locations = useLocation();
-    return(
-      <div>
-        <h3>Page not found at {locations.pathname}</h3>
-      </div>
-    );
-  }
-
-
-
-/*
-const [eventsData, setEventsData] = useState({})
-const getEventsData = () => { 
-  firebase.firestore().collection("calendat_data").get().then((snapshot) => {
-    const events = snapshot.docs.map(event => event.data());
-    setEventsData(events)
-    console.log(events)
-  }).catch((e) => {
-    console.log(e + "fetching error")
-  })
+export const Error = () => { //에러페이지
+  const locations = useLocation();
+  return (
+    <div>
+      <h3>Page not found at {locations.pathname}</h3>
+    </div>
+  );
 }
-useEffect(() => {
-   getEventsData()
- },[])
-*/
 
-const calendar_data = firestore.collection("calendar_data");
 const today = new Date();
-const Calendar = (props) => {
+const calendar_data = firestore.collection("calendar_data");
+
+const Calendar = () => {
   const [visible, setVisible] = useState(false);
   const [date, setDate] = useState(today);
+
+  const [eventsData, setEventsData] = useState({})
+  const getEventsData = () => {
+    calendar_data.get().then((snapshot) => {
+      const events = snapshot.docs.map(event => event.data());
+      setEventsData(events)
+      console.log(events)
+    }).catch((e) => {
+      console.log(e + "fetching error")
+    })
+  }
+  useEffect(() => {
+    getEventsData()
+  }, [])
+
+
   const handleDateSelect = (newDate) => {
     setDate(newDate);
-    setVisible(false);
+    setVisible(true);
   };
 
   const handleDateClick = (arg) => { // bind with an arrow function
-    console.log(arg)
-      var event = prompt("일정을 입력하세요.",);
-      calendar_data.doc(arg.dateStr).set( { date : arg.dateStr , title : `${event}`})
+    console.log(arg);
+
+    //<Modal open={modalopen} close={closeModal} header="Modal heading"></Modal>
+    var event = prompt("일정을 입력하세요.",);;
+    if (event) {
+      calendar_data.doc(arg.dateStr).set({ date: arg.dateStr, title: `${event}` })
+      console.log("데이터가 추가되었습니다.")
+    }
+    else
+      console.log("데이터가 null입니다. 추가되지 않았습니다.")
   }
 
   // 클릭 시 이벤트 정보 받아옴
   const handleEventClick = (clickInfo) => {
     console.log(clickInfo.event.id) // id 값 나옴    
-    }
+  }
 
   return (
-    <div className='Calendar'>
       <FullCalendar
-        plugins={[dayGridPlugin,interactionPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         headerToolbar={{
           left: "prev,next today",
           center: "title",
           right: "dayGridMonth,dayGridWeek,dayGridDay",
         }}
-        selectable = {true}
+        selectable={true}
         dateClick={handleDateClick}
         eventClick={handleEventClick}
         select={handleDateSelect}
         editable={true}
         droppable={true}
         weekends={true}
-        events={productData}
+        events={eventsData}
+        locale='ko'
+
       />
-      </div>
   );
 };
+
 export default Calendar;
